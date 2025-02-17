4010df24be5b7b397278f005ce30c1510ceb289b
4dr14n099 <adrianhenriquefarias111@gmail.com>
2025-02-17 17:24:26 -0300

    base virou b

--- src/exercicioo.java
+++ src/exercicioo.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class exercicioo{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        float base;
+        float b;
         float alt;
         float a;
 
         System.out.println("Informe a base do retângulo: ");
-        base = scanner.nextFloat();
+        b = scanner.nextFloat();
 
         System.out.println("Informe a altura do retângulo: ");
         alt = scanner.nextFloat();
 
-        a = base * alt;
+        a = b * alt;
 
         System.out.println("A área do retângulo é: " + a);
 
